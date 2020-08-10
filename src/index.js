8f53cab524d03e14cd68edf306ac22c7c1f0a073
Yevhenii Sahanenko <panakin05@gmail.com>
2020-08-10 20:45:21 +0300

    37_bind_of_methods

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import * as serviceWorker from './serviceWorker';
-import {state}  from './redux/state'
-import {rerenderEntireTree} from "./render";
+import store  from './redux/state'
+import ReactDOM from "react-dom";
+import React from "react";
+import {App} from "./App";
+import {BrowserRouter} from "react-router-dom";
+import './index.css';
 
-rerenderEntireTree(state)
+const rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App
+                state={state}
+                addPost={store.addPost.bind(store)}
+                updateNewPostText={store.updateNewPostText.bind(store)}
+                addMessage={store.addMessage.bind(store)}
+                updateNewMessageText={store.updateNewMessageText.bind(store)}
+            />
+        </BrowserRouter>
+        ,
+        document.getElementById('root'))
+}
+
+rerenderEntireTree(store.getState())
+
+store.subscribe(rerenderEntireTree)
+
+
 
 serviceWorker.unregister();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,84 +1,89 @@
-import {rerenderEntireTree} from "../render";
-
-export const state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hi my name is Jack', likesCount: 999},
-            { id: 2, message: 'lorem ipsum', likesCount: 123},
-            { id: 3, message: 'It\'s my first post', likesCount: 1},
-        ],
-        newProfileText: ''
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hi my name is Jack', likesCount: 999},
+                { id: 2, message: 'lorem ipsum', likesCount: 123},
+                { id: 3, message: 'It\'s my first post', likesCount: 1},
+            ],
+            newProfileText: ''
+        },
+        messagesPage: {
+            dialogs: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Andrew'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Viktor'},
+                {id: 6, name: 'Valera'}
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your it-kamasutra?'},
+                {id: 3, message: 'Yo'},
+                {id: 4, message: 'Yo'},
+                {id: 5, message: 'Yo'}
+            ],
+            newMessageText: ''
+        },
+        sidebar: {
+            friends: [
+                {
+                    id: 1,
+                    name: 'Sasha',
+                    url: 'https://www.freepngimg.com/thumb/shape/30004-8-circle-image.png'
+                },
+                {
+                    id: 2,
+                    name: 'Jenia',
+                    url: 'https://www.freepngimg.com/thumb/shape/30004-8-circle-image.png'
+                },
+                {
+                    id: 3,
+                    name: 'Ania',
+                    url: 'https://www.freepngimg.com/thumb/shape/30004-8-circle-image.png'
+                }
+            ]
+        }
     },
-    messagesPage: {
-        dialogs: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Andrew'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Viktor'},
-            {id: 6, name: 'Valera'}
-        ],
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How is your it-kamasutra?'},
-            {id: 3, message: 'Yo'},
-            {id: 4, message: 'Yo'},
-            {id: 5, message: 'Yo'}
-        ],
-        newMessageText: ''
+    getState() {
+      return this._state;
     },
-    sidebar: {
-        friends: [
-            {
-                id: 1,
-                name: 'Sasha',
-                url: 'https://www.freepngimg.com/thumb/shape/30004-8-circle-image.png'
-            },
-            {
-                id: 2,
-                name: 'Jenia',
-                url: 'https://www.freepngimg.com/thumb/shape/30004-8-circle-image.png'
-            },
-            {
-                id: 3,
-                name: 'Ania',
-                url: 'https://www.freepngimg.com/thumb/shape/30004-8-circle-image.png'
-            }
-        ]
+    _callSubscriber() {
+        console.log('ss')
+    },
+    addPost() {
+        const newPost = {
+            id: 6,
+            message: this._state.profilePage.newProfileText,
+            likesCount: 777
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newProfileText = ''
+        this._callSubscriber(this._state)
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newProfileText = newText;
+        this._callSubscriber(this._state)
+    },
+    addMessage() {
+        const newMessage = {
+            id: 6,
+            message: this._state.messagesPage.newMessageText,
+        }
+        this._state.messagesPage.messages.push(newMessage)
+        this._state.messagesPage.newMessageText = ''
+        this._callSubscriber(this._state)
+    },
+    updateNewMessageText(newText) {
+        this._state.messagesPage.newMessageText = newText;
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer
     }
 }
 
-window.state = state;
+export default store
 
-export let addPost = () => {
-    const newPost = {
-        id: 6,
-        message: state.profilePage.newProfileText,
-        likesCount: 777
-    }
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newProfileText = ''
-    rerenderEntireTree(state)
-}
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newProfileText = newText;
-    rerenderEntireTree(state)
-}
-
-export let addMessage = () => {
-    const newMessage = {
-        id: 6,
-        message: state.messagesPage.newMessageText,
-    }
-    console.log(newMessage)
-    console.log(state.messagesPage.messages)
-    state.messagesPage.messages.push(newMessage)
-    state.messagesPage.newMessageText = ''
-    rerenderEntireTree(state)
-}
-
-export let updateNewMessageText = (newText) => {
-    state.messagesPage.newMessageText = newText;
-    rerenderEntireTree(state)
-}
+window.store = store
