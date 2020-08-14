d6acba3ea690b6cdf18f81bf283fb045175e738b
Yevhenii Sahanenko <panakin05@gmail.com>
2020-08-14 11:39:16 +0300

    52_Users_add_button

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,13 +3,18 @@ import styles from './users.module.css';
 import axios from 'axios'
 import userPhoto from '../../assets/images/userPhoto.png'
 let Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(
-            (response) => props.setUsers(response.data.items)
-        )
+    let getUsers = () =>
+    {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then( response => {
+                    props.setUsers(response.data.items)
+                });
+        }
     }
 
     return <div>
+        <button onClick={getUsers}>get users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
